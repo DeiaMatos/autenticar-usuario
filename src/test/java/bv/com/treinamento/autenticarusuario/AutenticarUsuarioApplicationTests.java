c39e9fb2a90d5af30bd6d14dba59a2fd680c83ac
Deia Matos <dei-amattos@hotmail.com>
2021-04-10 19:07:37 -0300

    Aula 10/04 Spring Autenticar o usuário

---
+++ src/test/java/bv/com/treinamento/autenticarusuario/AutenticarUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package bv.com.treinamento.autenticarusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticarUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
